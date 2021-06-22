a6951c3b86e24c1744a6bbb255149af3bea8b669
halilatilla <atillahalil0@gmail.com>
2021-06-22 14:33:11 +0300

    Update colorSet default props

--- src/index.tsx
+++ src/index.tsx
@@ -68,24 +68,7 @@ TextParticles.defaultProps = {
   flowOffset: 0,
   gravity: { direction: 90, force: 0 },
   canvas: { width: 300, height: 300, bg: 'white' },
-  colorSet: [
-    '#f44336',
-    '#e91e63',
-    '#9c27b0',
-    '#673ab7',
-    '#3f51b5',
-    '#2196f3',
-    '#03a9f4',
-    '#00bcd4',
-    '#009688',
-    '#4CAF50',
-    '#8BC34A',
-    '#CDDC39',
-    '#FFEB3B',
-    '#FFC107',
-    '#FF9800',
-    '#FF5722'
-  ]
+  colorSet: ['#f44336','#e91e63','#9c27b0','#673ab7','#3f51b5']
 }
 
 export default TextParticles;
